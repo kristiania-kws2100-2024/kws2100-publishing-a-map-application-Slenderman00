d5a1822ff088b6126945780b5e7ca0811afc2d72
Joar Heimonen <joarheimonen@live.no>
2024-02-15 02:32:35 +0100

    Project can now handle multiple maps

--- src/mapComponent.tsx
+++ src/mapComponent.tsx
@@ -1,16 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import './ol.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import { OSM } from 'ol/source';
 
 const MapComponent: React.FC = () => {
+    //We are giving it a random id to allow for the possibility of multiple maps.
+    const _id = 'map' + (Math.floor((Math.random() * 9999) + 1)).toString();
 
     //This is dumb. The whole point of component based development is for the components to be independent and avoid using the global DOM id space.
     useEffect(() => {
-
         let map = new Map({
-            target: 'map',
+            target: _id,
             layers: [
                 new TileLayer({
                     source: new OSM()
@@ -29,7 +30,8 @@ const MapComponent: React.FC = () => {
 
     }, []);
 
-    return <div id="map" style={{width: '100%', height: '400px', border: '4px solid red'}} />;
+
+    return <div id={_id} style={{width: '100%', height: '400px', border: '4px solid red'}} />;
 };
 
 
